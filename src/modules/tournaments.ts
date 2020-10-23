e61c9bd3f2432bee79cc7e1dcc23cf8749e406d2
Carim A <CarimA@users.noreply.github.com>
2020-10-23 19:14:50 +0100

    Midnight rollover bugfix

--- src/modules/tournaments.ts
+++ src/modules/tournaments.ts
@@ -37,7 +37,6 @@ export default class TournamentsModule implements Module {
 		this._votingPhase = false;
 		this._room = process.env['PSIM_TOUR_ROOM'] ? process.env['PSIM_TOUR_ROOM'] : 'littlecup';
 
-		this.scheduleTournament(0);
 		this.scheduleTournament(4, 'lc');
 		this.scheduleTournament(12);
 		this.scheduleTournament(14, 'lc');
@@ -45,6 +44,7 @@ export default class TournamentsModule implements Module {
 		this.scheduleTournament(18, 'lc');
 		this.scheduleTournament(20, 'lcuu');
 		this.scheduleTournament(22);
+		this.scheduleTournament(24);
 	}
 
 	private scheduleTournament(hour: number, format?: string | undefined) {
