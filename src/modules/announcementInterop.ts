a5b6b5693a584b46f7369e1950c338fc1d2b9414
Carim A <CarimA@users.noreply.github.com>
2020-11-06 09:40:20 +0000

    Process Announcement Markdown

--- src/modules/announcementInterop.ts
+++ src/modules/announcementInterop.ts
@@ -1,6 +1,9 @@
 import * as Psim from 'ts-psim-client';
 import * as Discord from 'discord.js';
 import Module from '../module';
+import * as Marked from 'marked';
+import sanitizeHtml from 'sanitize-html';
+import * as HTMLMinifier from 'html-minifier';
 
 export default class AnnouncementInteropModule implements Module {
 	private _psimClient: Psim.Client;
@@ -24,10 +27,13 @@ export default class AnnouncementInteropModule implements Module {
 			const color = member ? member.displayHexColor : '#ffffff';
 			const avatar = message.author.displayAvatarURL();
 
+			const md = message.content;
+			const html = Marked.parse(md);
+			const sanitised = sanitizeHtml(html);
+			const minified = HTMLMinifier.minify(sanitised);
+
 			await room?.send(
-				`/adduhtml ${Date.now()}, <p><span style="font-size: 1rem">Discord Announcement (<a href="https://discord.gg/pjN29Dh">LC Discord Server</a>)</span><br><img src="${avatar}" width="18" height="18" style="vertical-align:bottom;border-radius:50%"> <strong style="color:${color}">${nickname}</strong></p><p>${
-					message.content
-				}</p>`
+				`/adduhtml ${Date.now()}, <p><span style="font-size: 1rem">Discord Announcement (<a href="https://discord.gg/pjN29Dh">LC Discord Server</a>)</span><br><img src="${avatar}" width="18" height="18" style="vertical-align:bottom;border-radius:50%"> <strong style="color:${color}">${nickname}</strong></p><p>${minified}</p>`
 			);
 		}
 	}
