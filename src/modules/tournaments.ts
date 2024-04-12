ba624ec71d6a4bfb26d5a29c9b182e6f925e056d
Carim A <CarimA@users.noreply.github.com>
2024-04-12 16:26:43 +0100

    Update tournaments.ts

--- src/modules/tournaments.ts
+++ src/modules/tournaments.ts
@@ -76,12 +76,18 @@ export default class TournamentsModule implements Module {
 		this._votingPhase = false;
 		this._room = process.env['PSIM_TOUR_ROOM'] ? process.env['PSIM_TOUR_ROOM'] : 'littlecup';
 
+		this.scheduleTournament(0, 'gen9lc');
 		this.scheduleTournament(2);
 		this.scheduleTournament(4, 'gen9lc');
-		this.scheduleTournament(11);
-		this.scheduleTournament(13, 'gen9lc');
-		this.scheduleTournament(15);
-		this.scheduleTournament(18, 'gen9lc');
+		this.scheduleTournament(6);
+		this.scheduleTournament(8, 'gen9lc');
+		this.scheduleTournament(10);
+		this.scheduleTournament(12, 'gen9lc');
+		this.scheduleTournament(14);
+		this.scheduleTournament(15, 'gen9lc');
+		this.scheduleTournament(16);
+		this.scheduleTournament(17, 'gen9lc');
+		this.scheduleTournament(18);
 		this.scheduleTournament(20, 'gen9doubleslc');
 		this.scheduleTournament(22, 'gen9lc');
 	}
